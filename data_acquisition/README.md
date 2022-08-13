5088e7cf3aba46f6cb665b46a443613b8bec66c0
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2022-08-14 01:35:38 +0400

    working database no test

---
+++ data_acquisition/README.md
@@ -0,0 +1,3 @@
+# Data Acquisition system to record dataset of keyboard sounds
+
+Usage: `python3 get_key_sample.py` to start recording
--- data_acquisition/get_key_sample.py
+++ data_acquisition/get_key_sample.py
@@ -1,21 +1,29 @@
+
+### Imports here! ###
 from datetime import datetime
 import sounddevice as sd
 from scipy.io.wavfile import write
 import numpy as np
 from pynput import keyboard
 import time
+import pandas as pd
+import os.path
+import argparse,logging, sys # All nice things
 
-### Settings imported here ###
-
-
-### Settings ###
+### Temp or default Settings ###
 audio_sample_rate = 44100  # Sample rate
 sample_duration = 0.3  # Duration of sub recording in a sample
 recordings_count = 3 # Number if sub recordings in a sample
 recordings_after_the_press = 2 # Delay after the press
 
 
-samples_folder = "samples_dataset"
+dataset_folder = "samples_dataset"
+database_file_name = "dataset.csv"
+
+### Settings imported here ###
+
+# No settings file yet!
+
 
 
 ### Global handler variables ###
@@ -26,8 +34,7 @@ record_flag = True
 
 
 ### Logging ###
-import logging
-import sys
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 log_handler = logging.StreamHandler(sys.stdout)
@@ -39,10 +46,12 @@ logger.addHandler(log_handler)
 
 class Button_Sample():
     '''Class for getting a button press from the numpy array and key stroke information'''
-    def __init__(self, id, recordings,button_key,sample_rate = 44100):
-        self.button_key = button_key
+    def __init__(self, id, recordings,button_key,dataset_folder="samples_dataset" ,sample_rate = 44100,comment=""):
+        self.comment = "" # Comment for database (if present)
+        self.button_key = button_key # Key that was pressed
         self.sample_rate = sample_rate
-        self.id = id
+        self.dataset_folder = dataset_folder # Folder with dataset
+        self.id = id # ID for database
         self.time_created = int(time.time())
         if len(recordings) > 1:
             self.full_recording = np.concatenate(recordings)
@@ -50,12 +59,12 @@ class Button_Sample():
             self.full_recording = recordings[0]
         else:
             raise ValueError('Empty recording list passed to the Button_Sample class.')
+        self.num_samples = len(self.full_recording)
         self.save_file()
         
     def save_file(self):
-        global samples_folder
-        path = f'{samples_folder}/{self.id}.wav'
-        write(path, self.sample_rate, self.full_recording)  # Save as WAV file 
+        self.path = f'{self.dataset_folder}/{self.id}.wav'
+        write(self.path, self.sample_rate, self.full_recording)  # Save as WAV file 
         
 
 def on_press(key):
@@ -111,15 +120,58 @@ def record_next_press():
     # print(recordings)
     return recordings
 
+### Functions to work with database ###
 
-# def read_database(path="database.csv"):
+class Sounds_Database():
+    def __init__(self,path_to_csv,dataset_folder="samples_dataset"):
+        self.dataset_folder = dataset_folder
+        self.dataframe = self.read_database(path_to_csv)
+        self.path = path_to_csv
+        if not os.path.isdir(self.dataset_folder):
+            os.mkdir(self.dataset_folder)
+        
+    def read_database(self,path="database.csv"):
+        '''Init database'''
+        print(f"PATH IS: {path}")
+        if os.path.isfile(path):
+            database = pd.read_csv(path)
+        else:
+            # Creating empty database from shema
+            schema = ["id","path","key_pressed","num_of_samples","time_created","comment"]
+            database = pd.DataFrame(columns =schema)
+        self.path = path
+        return database
+
+    def update_on_disk(self,backup=False):
+        self.dataframe.to_csv(self.path,index=False)
     
-#     return database
-
 
 if __name__ == '__main__':
     
-    time.sleep(0.4)
+    parser = argparse.ArgumentParser(description='Data Acquisition system to record dataset of keyboard sounds')
+    parser.add_argument('-d','--csv', type=str , help="Path for database")
+    parser.add_argument('--datasetpath', type=str , help="Path for dataset")
+    parser.add_argument('--nodatabase', type=str , help="Use to not create database files")
+    args = parser.parse_args()
+    
+    if args.datasetpath:
+        dataset_folder = args.datasetpath
+    
+    if args.csv:
+        database_file_name = args.csv
+    database = Sounds_Database(database_file_name,dataset_folder=dataset_folder)
+    
+    
+    
+    # Parse database
+    ## Get index from the database!
+    
+    if args.nodatabase or len(database.dataframe) == 0:
+        index = 0
+    else:
+        index = int(database.dataframe.iloc[-1:]["id"]) + 1
+    
+    time.sleep(0.2) # Delay to avoid starting to early
     exit_command = False
     logging.info(f'Recording of key presses started')
     listener = keyboard.Listener(
@@ -127,12 +179,29 @@ if __name__ == '__main__':
         on_release=on_release)
     listener.start()
     logging.info(f'Listener started')
-    index = 0
+    
+    
+
     while not exit_command:
         sample = record_next_press()
         if not exit_command:
-            button_press = Button_Sample(index,sample,key_pressed)
-        time.sleep(0.1)
+            # index += 1 # Iterate index since index = last index used
+            button_press = Button_Sample(index,sample,key_pressed,dataset_folder=dataset_folder)
+            new_database_entery = {
+                "id":index,"path":button_press.path,
+                "key_pressed":button_press.button_key,
+                "num_of_samples":button_press.num_samples,
+                "time_created":button_press.time_created,
+                "comment":button_press.comment
+            }
+            temp_df = pd.DataFrame(new_database_entery,index=[0])
+            # logging.info(f"New entery: {pd.DataFrame(new_database_entery,index=[0])}")
+            if not args.nodatabase:
+                database.dataframe = pd.concat([database.dataframe,temp_df])
+                database.update_on_disk()
+                
         index += 1
+        time.sleep(0.1) # Small delay here to avoid double taps
+        
         
     
