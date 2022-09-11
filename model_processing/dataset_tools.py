4804f2d031f88c6ff67c8aaf16d146e6ea5a99bd
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2022-09-11 14:32:09 +0400

    working nn

---
+++ model_processing/dataset_tools.py
@@ -0,0 +1,95 @@
+# File with support code for working with dataset
+
+### Imports here! ###
+from datetime import datetime
+import numpy as np
+# from pynput import keyboard
+import time
+import pandas as pd
+import os.path
+import argparse,logging, sys # All nice things
+
+
+import math, random
+import torch
+import torchaudio
+from torchaudio import transforms
+from IPython.display import Audio
+
+class AudioUtil():
+    # ----------------------------
+    # Load an audio file. Return the signal as a tensor and the sample rate
+    # ----------------------------
+    @staticmethod
+    def open(audio_file):
+        sig, sr = torchaudio.load(audio_file)
+        return (sig, sr)
+    # ----------------------------
+    # Generate a Spectrogram
+    # ----------------------------
+    @staticmethod
+    def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
+        sig,sr = aud
+        top_db = 80
+
+        # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+        spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
+
+        # Convert to decibels
+        spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
+        return (spec)
+    
+    
+class SoundDS(torch.utils.data.Dataset):
+    def __init__(self, df, data_path):
+        self.df = df
+        self.test_keys = get_unique_keys(df)
+        self.data_mapper = preprocessing.LabelEncoder()
+        targets = self.data_mapper.fit_transform(self.test_keys)
+        # targets: array([0, 1, 2, 3])
+        targets = torch.as_tensor(targets)
+        self.data_path = str(data_path)
+        # self.duration = 4000
+        self.sr = 44100
+        self.channel = 2
+        self.shift_pct = 0.4
+              
+    # ----------------------------
+    # Number of items in dataset
+    # ----------------------------
+    def __len__(self):
+        return len(self.df)    
+      
+    # ----------------------------
+    # Get i'th item in dataset
+    # ----------------------------
+    def __getitem__(self, idx):
+        # Absolute file path of the audio file - concatenate the audio directory with
+        # the relative path
+        # audio_file = self.data_path + self.df.loc[idx, 'path']
+        audio_file = os.path.join( self.data_path ,  self.df.loc[idx, 'path'])
+        # Get the Class ID
+        class_id = self.data_mapper.transform( [self.df.loc[idx, 'key_pressed']] )[0]
+
+        aud = AudioUtil.open(audio_file)
+        # Some sounds have a higher sample rate, or fewer channels compared to the
+        # majority. So make all sounds have the same number of channels and same 
+        # sample rate. Unless the sample rate is the same, the pad_trunc will still
+        # result in arrays of different lengths, even though the sound duration is
+        # the same.
+        # reaud = AudioUtil.resample(aud, self.sr)
+        # rechan = AudioUtil.rechannel(reaud, self.channel)
+
+        # dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
+        # shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
+        sgram = AudioUtil.spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None)
+        # aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+
+        return sgram, class_id
+  
+def get_unique_keys(df):
+    keys = []
+    for key in df['key_pressed']:
+        if key not in keys:
+            keys.append(key)
+    return keys  
--- model_processing/utils.py
+++ model_processing/utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
+import os,sys
+import torch
 
 def plot_time_data(data,save_figure_path=None,figsize=(7, 5)):
        fig = plt.figure(figsize=figsize, dpi= 100, facecolor='w', edgecolor='k')
@@ -52,3 +54,25 @@ def plot_fft_data_plotly(freq,data,left_part=True,xlog=True,ylog=False):
 def plot_time_data_plotly(time,data,xlog=False,ylog=False):
     fig = px.line(x=time, y=data, labels={'x':'Freq (Hz)', 'y':'Magnutude'},log_x=xlog,log_y=ylog)
     fig.show()
+    
+    
+def save_model(model, dataset = None , name = None , comment = "", models_folder = "models"):
+    
+    if not os.path.exists(models_folder):
+        os.mkdir(models_folder)
+    if not  os.path.exists("models_list.csv"):
+        pd.DataFrame(columns=["model_name", "filename", "accuracy", "loss", "num_samples", "num_epochs", "date" , "comment"]).to_csv("models_list.csv", index=False)
+    models_dataframe = pd.read_csv("models_list.csv")
+
+    if dataset is not None:
+        num_samples = len(dataset)
+        
+    else:
+        num_samples = None
+    filename = os.path.join(models_folder,f"{model.date}.pt")
+    torch.save(model, filename)
+    model_row = { "model_name": name, "filename": filename, "accuracy": model.acc, "loss": model.loss, "num_samples": num_samples, "num_epochs": num_epochs , "date": model.date, "comment": comment}
+    models_dataframe = models_dataframe.append(model_row, ignore_index=True).to_csv("models_list.csv", index=False)
+    
+def load_model(filename):
+       return torch.load(filename)
