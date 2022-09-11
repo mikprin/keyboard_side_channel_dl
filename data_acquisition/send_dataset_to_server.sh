0ef75ea99df60bee0499588b4141ec3d592c51ec
mikprin <miksolo@yandex.ru>
2022-09-11 16:01:26 +0400

    new script

---
+++ data_acquisition/send_dataset_to_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+$SCRIPT_PATH/create_database_bkup.sh
+
+SERVER_PATH=/6TB/DATASET/laptop_keyboard_press/dataset.tar.gz
+
+rsync -av  -e 'ssh -p 2224' "$SCRIPT_PATH/bkup/dataset.tar.gz"  miksolo@miksolo.xyz:$SERVER_PATH
